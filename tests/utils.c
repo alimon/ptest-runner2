ccc38b25583bc41dc149bfb0311a21f37fcabcef
Aníbal Limón <anibal.limon@linux.intel.com>
2016-01-13 17:31:41 -0600

    tests/utils.c: Add full testcase for run_ptests.
        - Case all tests.
        - Case of hang also adds data for simulate hang.
        - Case for read data in stderr.
    
    Signed-off-by: Aníbal Limón <anibal.limon@linux.intel.com>

--- tests/utils.c
+++ tests/utils.c
@@ -39,10 +39,11 @@ static char *ptests_found[] = {
 	"bash",
 	"gcc",
 	"glibc",
+	"hang",
 	"python",
 	NULL
 };
-static int ptests_found_length = 4;
+static int ptests_found_length = 5;
 static char *ptests_not_found[] = {
 	"busybox",
 	"perl",
@@ -135,6 +136,67 @@ START_TEST(test_filter_ptests)
 	ptest_list_free_all(head_new);
 END_TEST
 
+START_TEST(test_run_ptests)
+	struct ptest_list *head; 
+	int timeout = 1;
+	int rc;
+
+	char *buf_stdout;
+	size_t size_stdout = PRINT_PTEST_BUF_SIZE;
+	FILE *fp_stdout;
+	char *buf_stderr;
+	size_t size_stderr = PRINT_PTEST_BUF_SIZE;
+	FILE *fp_stderr;
+
+	int found_timeout = 0;
+	int next = 0;
+	const char *ptest_hang = "hang";
+	const char *timeout_str = "TIMEOUT";
+	char *line;
+	char line_buf[PRINT_PTEST_BUF_SIZE];
+
+	fp_stdout = open_memstream(&buf_stdout, &size_stdout);
+	ck_assert(fp_stdout != NULL);
+	fp_stderr = open_memstream(&buf_stderr, &size_stderr);
+	ck_assert(fp_stderr != NULL);
+
+	head = get_available_ptests(opts_directory);
+	ptest_list_remove(head, "hang", 1);
+	rc = run_ptests(head, timeout, "test_run_ptests", fp_stdout, fp_stderr);
+	ck_assert(rc == 0);
+	ptest_list_free_all(head);
+
+	head = get_available_ptests(opts_directory);
+	rc = run_ptests(head, timeout, "test_run_ptests", fp_stdout, fp_stderr);
+
+	/* Search for TIMEOUT keyword in output of hang ptest */
+	while ((line = fgets(line_buf, PRINT_PTEST_BUF_SIZE, fp_stdout)) != NULL) {
+		if (next) {
+			line[strlen(timeout_str)] = '\0'; 
+			if (strcmp(line, timeout_str) == 0)
+				found_timeout = 1;
+		} else {
+			line[strlen(ptest_hang)] = '\0'; // XXX: Only compare the name part
+			if (strcmp(line, ptest_hang) == 0)
+				next = 1;
+		}
+	}
+	ck_assert(found_timeout == 1);
+
+	/* Test stderr output */
+	line = fgets(line_buf, PRINT_PTEST_BUF_SIZE, fp_stderr);
+	line[strlen(ptest_hang)] = '\0';
+	ck_assert(strcmp(line, ptest_hang) == 0);
+
+	ck_assert(rc != 0);
+	ptest_list_free_all(head);
+
+	fclose(fp_stdout);
+	free(buf_stdout);
+	fclose(fp_stderr);
+	free(buf_stderr);
+END_TEST
+
 Suite *
 utils_suite()
 {
@@ -147,6 +209,7 @@ utils_suite()
 	tcase_add_test(tc_core, test_get_available_ptests);
 	tcase_add_test(tc_core, test_print_ptests);
 	tcase_add_test(tc_core, test_filter_ptests);
+	tcase_add_test(tc_core, test_run_ptests);
 
 	suite_add_tcase(s, tc_core);
 
